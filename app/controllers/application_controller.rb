28d880802aae8f9f4e2ee1a68b1e204486c6b9d0
kanu-e <chukwuemekakanu@gmail.com>
2021-04-10 01:27:39 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,31 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    # register Sinatra::Flash
+    enable :sessions
+    set :session_secret, "secrets"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+  
+    def current_user
+      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+  end
+
+  def authorize(tweet)
+    authenticate
+    if tweet.user != current_user
+      redirect '/tweets'
+    end
   end
 
 end
